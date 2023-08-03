3115ba0c3beda847d2f7cf2a41a5709c05da96cd
amangoyal018 <goyalamanmay18@gmail.com>
2023-08-03 09:39:34 +0530

    searching starts

---
+++ Searching/basic.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    int k;
+    cin>>k
+    // bool ans =false;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        // cout<<arr[i]<<"\n";
+    }
+    
+
+    
+
+}
