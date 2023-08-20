f5d04a68cea7e37dedd1b29e028b1182f7e6950c
amangoyal018 <goyalamanmay18@gmail.com>
2023-08-21 04:21:32 +0530

    Searching to hai

---
+++ Searching/allocate_min_pages2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> min_pages(int *arr ,int n,int k){
+    
+
+
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+
+    int k;
+    cin>>k;
+    int arr[n];
+
+    int sum=0;
+    int max1=INT_MIN;
+    
+    
+    
+
+    
+
+}
