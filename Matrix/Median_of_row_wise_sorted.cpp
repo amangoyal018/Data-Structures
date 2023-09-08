6dc0db777875f0d644995d2978751625f0fc0d75
amangoyal018 <goyalamanmay18@gmail.com>
2023-09-08 19:43:24 +0530

    Matrix Done

--- Matrix/Median_of_row_wise_sorted.cpp
+++ Matrix/Median_of_row_wise_sorted.cpp
@@ -41,6 +41,10 @@ int main()
 
     int  n , m;
     cin >> n >> m ;
+
+
+    int max1 = INT_MIN;
+    int min1 = INT_MAX;
     
 
     int **arr = new int *[n];
@@ -50,8 +54,31 @@ int main()
     for( int i = 0 ; i < n ; i++ ){
         for( int j = 0 ; j < m ; j++){
             cin>>arr[i][j];
+            min1 = min( min1 , arr[i][j]);
+            max1 = max( max1 , arr[i][j]);
         }
     }
+    int medPos = (n*m+1)/2;
+    
+    while(min1<max1){
+        int mid = (min1 + max1)/2;
+        int midPos = 0;
+        for(int i=0;i<n;i++){
+            midPos += upper_bound(arr[i],arr[i]+m,mid) - arr[i];
+
+        }
+        // cout << midPos;
+        if(midPos<medPos){
+            min1 = mid+1;
+
+        }else{
+            max1 = mid;
+        }
+        
+    }
+    cout<<min1;
+
+
     
     
     
--- Matrix/Search_in_row_and_coloumn_wise_sorted_2darray.cpp
+++ Matrix/Search_in_row_and_coloumn_wise_sorted_2darray.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
 void Transpose( int * arr[],int n ,int m)
 {
     for( int i = 0 ; i < n ; i++ )
@@ -24,12 +22,6 @@ void Print( int *arr[], int n , int m )
         cout << "\n" ;
     }
 }
-
-
-
-
-
-
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -41,17 +33,61 @@ int main()
 
     int  n , m;
     cin >> n >> m ;
-    
+
 
     int **arr = new int *[n];
-    for( int i = 0 ; i < n ; i++ ){
+
+    for( int i = 0 ; i < n ; i++ )
+    {
         arr[i] = new int [m];
     }
-    for( int i = 0 ; i < n ; i++ ){
-        for( int j = 0 ; j < m ; j++){
-            cin>>arr[i][j];
+
+
+    for( int i = 0 ; i < n ; i++ )
+    {
+        for( int j = 0 ; j < m ; j++)
+        {
+            cin >> arr[i][j] ;
         }
     }
+
+    int x ;
+    cin >> x;
+
+    //initialy selecting top right element in the 2d array
+    int r = 0;
+    int c = m-1;
+
+    vector < int > v;
+
+
+    while( r < n and c > -1){
+        if( x == arr[r][c] )
+        {
+            v.push_back(r);
+
+            v.push_back(c);
+
+            break;
+        }
+
+        if( x > arr[r][c]){
+            r++;
+        }else{
+            c--;
+        }
+
+    }
+    if( v.size() == 0 )
+    {
+        v.push_back(-1);
+    }
+    for( auto y : v )
+    {
+        cout << y << " " ;
+    }
+
+
     
     
     
