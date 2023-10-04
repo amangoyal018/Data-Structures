cba6216100b916796295554ae4c3a9ca59263415
amangoyal018 <goyalamanmay18@gmail.com>
2023-10-05 00:09:27 +0530

    kmp start

--- Strings/rabin_karp_algo.cpp
+++ Strings/rabin_karp_algo.cpp
@@ -17,16 +17,16 @@ int main()
     int m = ptrn.size();
 
 
-    int temp = ptrn[0]-'a'+1;
 
-    for(int i=1;i<m;i++){
-        temp+=ptrn[i]-'a'+1;
-    }
+    // int temp = ptrn[0]-'a'+1;
 
-    int val = str[0]-'a'+1;
-    for(int i=1;i<m;i++){
-        val+=str[i]-'a'+1;
-    }
+    // for(int i=1;i<m;i++){
+    //     temp+=ptrn[i]-'a'+1;
+    // }
+    // int val = str[0]-'a'+1;
+    // for(int i=1;i<m;i++){
+    //     val+=str[i]-'a'+1;
+    // }
     // cout<<val;
     
 
@@ -50,13 +50,31 @@ int main()
     //     }
     // }
 
+
+
     // better hash function 
     // d^m-1*ptrn[0]+d^m-2*ptrn[0]....d^0*ptrn[m-1]
+    int d;
+    d=5;
+
+    int temp = 0;
+
+    for(int i=0;i<m;i++){
+        temp+=round(pow(d,m-i-1))*(ptrn[i]-'a'+1);
+    }
     
+
+    int val=0;
+    for(int i=0;i<m;i++){
+        val+= round(pow(d,m-i-1))*(str[i]-'a'+1);
+    }
+    // cout<<val;
+
     for(int i = 0 ; i<n-m+1 ; i++){
         if(i!=0){
-            val+=str[i+m-1]-'a'+1;
-            val-=str[i-1]-'a'+1;
+            val-=(str[i-1]-'a'+1)*round(pow(d,m-1));
+            val*=d;
+            val+= str[i+m-1]-'a'+1;
         }
         // cout<<val<<"\n";
         
