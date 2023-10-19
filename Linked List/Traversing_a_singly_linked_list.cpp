e1dee2414cffb66373abc55e285e8351e2058282
amangoyal018 <goyalamanmay18@gmail.com>
2023-10-19 10:27:04 +0530

    LinkedList Started

--- Linked List/Traversing_a_singly_linked_list.cpp
+++ Linked List/Traversing_a_singly_linked_list.cpp
@@ -29,6 +29,16 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    Node *head = new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    printList(head);
+    
+
+
+
+    
 
     
     
--- Linked List/inser_at_begin_singly_linkedlist.cpp
+++ Linked List/inser_at_begin_singly_linkedlist.cpp
@@ -30,6 +30,16 @@ int main()
     cin.tie(NULL);
 
 
+    Node *head = new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+
+    printList(head);
+
+
+
+
     
     
 
